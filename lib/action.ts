e6bf429b639718e6541d9531e02050d469569492
Goudmu <goudmu@gmail.com>
2024-07-12 16:20:34 +0700

    revalidate

---
+++ lib/action.ts
@@ -0,0 +1,8 @@
+"use server";
+
+import { revalidatePath, revalidateTag } from "next/cache";
+
+export default async function revalidateAllPath() {
+  revalidatePath("/dashboard");
+  revalidatePath("/expenses");
+}
